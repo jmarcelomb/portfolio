767dcb525bcff534d52387c738209dc32a8ffa0f
Marcelo Borges <jmmb1098@gmail.com>
2024-01-28 16:31:52 +0000

    Uses main.rs without tailwind

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,38 @@
-mod app;
-
-use app::*;
 use leptos::*;
 
-pub fn main() {
-    _ = console_log::init_with_level(log::Level::Debug);
-    console_error_panic_hook::set_once();
+#[component]
+fn App(increment: i32) -> impl IntoView {
+    let (count, set_count) = create_signal(0);
 
-    logging::log!("csr mode - mounting to body");
+    view! {
+    <div class="container">
 
+            <picture>
+                <source srcset="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_pref_dark_RGB.svg" media="(prefers-color-scheme: dark)" />
+                <img src="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_RGB.svg" alt="Leptos Logo" height="200" width="400"/>
+            </picture>
+
+        <h1>"Welcome to Leptos"</h1>
+        <h2><i>"On Github Pages"</i></h2>
+
+        <button
+            on:click= move |_| {
+                set_count(count() + increment)
+            }
+        >
+            "Click me: "
+            {count}
+        </button>
+
+
+    </div>
+    }
+}
+
+fn main() {
     mount_to_body(|| {
-        view! { <App /> }
-    });
+        view! {
+            <App increment=5 />
+        }
+    })
 }
